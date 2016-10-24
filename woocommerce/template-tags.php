256b1ec2a6703dbcc5d210a5396e92ed8235595d
adiraomj <adiraomj@outlook.com>
2016-10-24 10:45:32 +0530

    Added basic styling to WooCommerce archive

--- woocommerce/template-tags.php
+++ woocommerce/template-tags.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 /**
  * Custom WooCommerce template tags for this theme.
  *
@@ -25,3 +25,18 @@ add_action( 'woocommerce_after_single_product_summary', 'siteorigin_unwind_wooco
 
 // Remove the cross sell display.
 remove_action( 'woocommerce_cart_collaterals', 'woocommerce_cross_sell_display' );
+
+function siteorigin_unwind_loop_image_div_open() {
+	echo '<div class="product-loop-image">';
+}
+function siteorigin_unwind_loop_image_div_close() {
+	echo '</div>';
+}
+// Inserting an anchor tag around product title.
+remove_action( 'woocommerce_shop_loop_item_title', 'woocommerce_template_loop_product_title' );
+add_action( 'woocommerce_shop_loop_item_title', 'siteorigin_unwind_loop_product_title', 10 );
+function siteorigin_unwind_loop_product_title() {
+	echo '<a href="' . get_the_permalink() . '"><h3>' . get_the_title() . '</h3></a>';
+}
+// Insert
+add_action( 'woocommerce_before_shop_loop_item_title', 'woocommerce_template_loop_add_to_cart', 10 );
